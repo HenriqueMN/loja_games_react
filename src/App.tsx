3e4c33e42d812e8ea1a384a5f9f706aa2e4128ed
Henrique M. N. <henriquemancininevado@gmail.com>
2024-01-23 19:43:02 -0300

    Implementação das rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Footer from "./components/footer/Footer"
 import NavBar from "./components/navbar/NavBar"
 import Home from "./pages/home/Home"
+import DeletarCategoria from "./categorias/deletarCategoria/DeletarCategoria"
+import FormularioCategoria from "./categorias/formularioCategoria/FormularioCategorias"
+import ListarCategorias from "./categorias/listarCategorias/ListarCategorias"
 
 
 function App() {
   return (
     <>
-    <NavBar />
-    <Home />
-    <Footer />
+    <BrowserRouter>
+      <NavBar />
+      <div className="min-h-[80vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/categorias" element={<ListarCategorias />} />
+          <Route path="/cadastroCategoria" element={<FormularioCategoria />} />
+          <Route path="/editarCategoria/:id" element={<FormularioCategoria />} 
+          />
+          <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} 
+          />  
+        </Routes>
+
+      </div>
+      <Footer />
+    </BrowserRouter>
     </>
   )
 }
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,12 +1,12 @@
-// import {Link, useNavigate} from "react-router-dom"
+import {Link, useNavigate} from "react-router-dom"
 import {ShoppingCart, User} from "@phosphor-icons/react"
 import videogame from "../../assets/videogame.png"
 
 function NavBar() {
 
-    // let navigate = useNavigate()
+    let navigate = useNavigate()
 
-    //let navbarComponent
+    let navbarComponent
 
     return (
         <>
@@ -14,13 +14,13 @@ function NavBar() {
             <div className="container flex justify-between text-lg items-center">
                 <div className="flex gap-4 items-center">
                 <img src={videogame} alt="Desenho de um controle de videogame com uma TV atrás e alguns jogos do lado" className="size-10"/>
-                <div className="uppercase text-xl font-semibold">Loja Games</div>
+                <Link to="/home" className="uppercase text-xl font-semibold">Loja Games</Link>
                 </div>
                  <input type="text" placeholder="Pesquisar" className="rounded w-2/5 h-fit"/>
                 <div className="flex gap-4">
                     <div className="hover:underline">Produtos</div>
-                    <div className="hover:underline">Categorias</div>
-                    <div className="hover:underline">Cadastrar Categoria</div>
+                    <Link to="/categorias" className="hover:underline">Categorias</Link>
+                    <Link to="/cadastroCategoria" className="hover:underline">Cadastrar Categoria</Link>
                     <div className="flex gap-2">
                         <User size={32} weight="bold" />
                         <ShoppingCart size={32} weight="bold" />
